8c2f7ee5083c6b2bb09ad1438e3ff47a82868a71
Theryston Santos <funktodo2@gmail.com>
2022-03-31 19:50:59 -0300

    fix: limit of page generation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 60 * 60, // 1 hour
   images: {
     domains: ["onedrive.live.com"],
   },
